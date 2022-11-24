1cbd4a15b86fa0ada30e3dd3aef3e212f6ca4217
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-11-24 21:29:07 +0000

    Working on the setup script:

--- setup/setup_digital_ocean.bash
+++ setup/setup_digital_ocean.bash
@@ -1,7 +1,17 @@
-# This script boostraps the dev environment so that
-# we can use Python to install everything else.
-# Note that this file is piped into bash, not sourced.
+# Bootstrap the setup of a digital ocean installation by:
+# 
+# 1. Installing the dotfiles repo
+# 2. Installing python3.10-venv if necessary
+# 3. Creating a venv
+# 4. Running a script in the venv
+#     - if root: setup_root.py
+#     - not root: setup_user.py
 
+# Variables
+GIT_REPO="git@github.com:treuille/dotfiles.git"
+GIT_BRANCH="unified"
+DOTFILES_PATH="dotfiles"
+SETUP_PATH="${DOTFILES_PATH}/setup"
 
 # Tell the user what we're doing at the start of each block.
 start_block()
@@ -18,56 +28,105 @@ end_block()
     echo -e "\e[32mDone\e[0m\n"
 }
 
-# Check whether we can log into github. If not, something is wrong.
-if [ ! -S "$SSH_AUTH_SOCK" ];
-then
-   echo "Unable to log into Github. Exiting."
-   exit 255
-fi
+# Install the dotfiles repo
+install_dotfiles()
+{
+  # Clone the repo
+  start_block "Cloning the repo"
+  if [[ -d ${DOTFILES_PATH} ]];
 
-# Clone the repo
-start_block "Cloning the repo"
-GIT_REPO="git@github.com:treuille/dotfiles.git"
-cd ~
-if [[ ! -d dotfiles ]];
-then
-    # TODO: GET RID OF THE UNIFIED BRANCH
-    git clone -b unified "$GIT_REPO"
-fi
-end_block
+  then
+    echo -e "\e[1m\e[31mRepo \"${DOTFILES_PATH}\" already exists.\e[0m"
 
-# The initial (root) install updates the apt repoe ans installs pipenv.
-if [[ $EUID -eq 0 ]];
-then
-    # A very existential statement.
-    echo -e "\e[1m\e[31mYou are root.\e[0m"
-    echo
+  else
+	 # Check whether we can log into github. If not, something is wrong.
+	 if [ ! -S "$SSH_AUTH_SOCK" ];
+	 then
+		echo "Unable to log into Github. Exiting."
+		exit 255
+	 fi
 
-    # Upgrade apt-get.
-    start_block "Setting up apt-get"
-    apt update -y
-    apt upgrade -y
-    apt install python3.10-venv -y
-    end_block
-else
-    echo -e "\e[1m\e[31mRunning as a non-root user.\e[0m"
-    echo
-fi
+	 # Actually clone the repo
+	 git clone -b ${GIT_BRANCH} ${GIT_REPO}
+  fi
+  end_block
+}
 
-bash ~/dotfiles/setup/run_python.bash setup_digital_ocean
-
-# echo "Setup the python environment." # <- REMOVE
+install_python_venv()
+{
+  # Only run in the user is root.
+  if [[ $EUID -ne 0 ]];
+  then
+	 return 255
+  fi
+  
+# 		# A very existential statement.
+# 		echo -e "\e[1m\e[31mYou are root.\e[0m"
+# 		echo
 # 
-# # Run the python setup script.
-# start_block "Setting up python environment"
-# cd ~/dev-env/
-# python3 -m venv .venv
-# .venv/bin/pip install -r requirements.txt
-# end_block
+# 		# Upgrade apt-get.
+# 		start_block "Setting up apt-get"
+# 		apt update -y
+# 		apt upgrade -y
+# 		apt install python3.10-venv -y
+# 		end_block
+#   else
+# 		echo -e "\e[1m\e[31mRunning as a non-root user.\e[0m"
+# 		echo
+#   fi
+
+  PYTHON_VENV_PACKAGE="python3.10-venv"
+  dpkg -s ${PYTHON_VENV_PACKAGE} &> /dev/null
+  if [[ $? -eq 0 ]];
+  then
+	 echo "The package ${PYTHON_VENV_PACKAGE} already exists."
+
+  else
+	 echo "The package ${PYTHON_VENV_PACKAGE} DOES NOT EXIST"
+
+  fi
+}
+
+# Actually run the script
+install_dotfiles
+install_python_venv
+exit 255
+
+
+
+exit 255
+
 # 
 # 
-# # Actualy run the sub command
-# echo -e "\e[1m\e[31mRunning Python setup script in $(pwd)\e[0m"
-# echo
-# .venv/bin/python ./setup_env.py
-# # pipenv run python ./setup_env.py
+# # The initial (root) install updates the apt repoe ans installs pipenv.
+# bash ~/dotfiles/setup/run_python.bash setup_digital_ocean
+# 
+# # echo "Setup the python environment." # <- REMOVE
+# # 
+# # # Run the python setup script.
+# # start_block "Setting up python environment"
+# # cd ~/dev-env/
+# # python3 -m venv .venv
+# # .venv/bin/pip install -r requirements.txt
+# # end_block
+# # 
+# # 
+# # # Actualy run the sub command
+# # echo -e "\e[1m\e[31mRunning Python setup script in $(pwd)\e[0m"
+# # echo
+# # .venv/bin/python ./setup_env.py
+# # # pipenv run python ./setup_env.py
+
+############################
+# RUNNING THE SCRIPT STUFF #
+############################
+
+# PYTHON_SCRIPT=${1}
+# SCRIPT_PATH="${SETUP_PATH}/${PYTHON_SCRIPT}.py"
+
+# # Check for the setup path.
+# if [[ ! -f ${SCRIPT_PATH} ]];
+# then
+#   echo "Missing script ${SCRIPT_PATH}."
+#   exit 255
+# fi
