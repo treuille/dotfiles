5c39ea536010f4dadf21b6dde14528e68699f0e6
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-11-25 08:06:42 -0800

    Starting to implement setup_user

--- setup/setup_user.py
+++ setup/setup_user.py
@@ -12,38 +12,6 @@ import setup_utils
 # import time
 # import tempfile
 
-def lock_down_ssh():
-    """Lock down ssh logins. Mostly, turn off root ssh login."""
-
-    sshd_config_filename = "/etc/ssh/sshd_config"
-    cprint(f"Updating {sshd_config_filename}...", "blue", attrs=["bold"])
-    if os.path.exists(sshd_config_filename):
-        config_changes = [
-            ("PasswordAuthentication yes", "PasswordAuthentication no"),
-            ("PermitRootLogin yes", "PermitRootLogin no"),
-        ]
-        sshd_config = open(sshd_config_filename).readlines()
-        changed_config = False
-        for line_number, config_line in enumerate(sshd_config):
-            for change_from, change_to in config_changes:
-                if config_line.strip() == change_from.strip():
-                    print(f"{line_number+1}: {change_from} -> {change_to}")
-                    sshd_config[line_number] = change_to + "\n"
-                    changed_config = True
-        print(f"changed config: {changed_config}")
-        if changed_config:
-            temp_file = tempfile.NamedTemporaryFile(mode="w", delete=False)
-            temp_file.writelines(sshd_config)
-            temp_file.close()
-            os.system(f"chmod -v --reference={sshd_config_filename} {temp_file.name}")
-            os.system(f"mv -v {temp_file.name} {sshd_config_filename}")
-            print(f"Overwrote {sshd_config_filename}")
-            print("Please restart the computer.")
-        cprint("Done\n", "green")
-    else:
-        cprint(f"Could not find {sshd_config_filename}\n", "cyan")
-
-
 def install_nvim_plugins():
     """Install the nvim and coc plugins."""
     # Would be great to figure out a way to do this automatically
@@ -56,7 +24,7 @@ def install_nvim_plugins():
         cprint("\nSkipping nvim plugins (couldn't read stdin).\n", "cyan")
 
 
-def both_install():
+def setup_user():
     """These are the packages which shoild happen for any user."""
 
     # Removing bash config files.
@@ -102,25 +70,13 @@ def both_install():
 
     install_nvim_plugins()
 
-
-
-
 def main():
     """Execution starts here."""
     # These installations require the user to be root.
     if setup_utils.user_is_root():
         raise RuntimeError("Should not run this script as root.")
 
-    setup_root()
-
-    # # Both root and non-root accounts are setup this way.
-    # # both_install()
-
-    # # Tell the root user to reboot.
-    # if setup_utils.user_is_root():
-    #     cprint(f"Please reboot the computer:", "red", attrs=["bold"])
-    #     print("  shutdown -r now")
-
+    setup_user()
 
 if __name__ == "__main__":
     main()
