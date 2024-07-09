e91ddd9fed800b773d5691435ef2df6015a6c2ef
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-07-09 00:09:20 -0700

    Updated branch name to tahiti-kickstart-nvim everywhere

--- setup/setup_digital_ocean.bash
+++ setup/setup_digital_ocean.bash
@@ -9,7 +9,7 @@
 
 # Variables
 GIT_REPO="git@github.com:treuille/dotfiles.git"
-GIT_BRANCH="tahiti"
+GIT_BRANCH="tahiti-kickstart-nvim"
 DOTFILES_PATH="dotfiles"
 SETUP_PATH="${DOTFILES_PATH}/setup"
 VENV_PATH="${DOTFILES_PATH}/setup/venv"
