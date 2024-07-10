230d7e25ca019788c77534dd1143078fd87d21a1
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-07-10 00:16:47 -0700

    Added the conform plugin

--- .config/nvim/lua/kickstart/plugins/conform.lua
+++ .config/nvim/lua/kickstart/plugins/conform.lua
@@ -26,9 +26,13 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
-        --
+
+        -- For Python, just using black, not isort
+        python = { "black" },
+
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
--- .config/nvim/lua/lazy-plugins.lua
+++ .config/nvim/lua/lazy-plugins.lua
@@ -10,17 +10,8 @@
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
-  --
-  -- Use `opts = {}` to force a plugin to be loaded.
-  --
-  --  This is equivalent to:
-  --    require('Comment').setup({})
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth', 
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
@@ -35,9 +26,9 @@ require('lazy').setup({
   require 'kickstart/plugins/telescope',
 
 --  require 'kickstart/plugins/lspconfig',
---
---  require 'kickstart/plugins/conform',
---
+
+  require 'kickstart/plugins/conform',
+
 --  require 'kickstart/plugins/cmp',
 
   require 'kickstart/plugins/tokyonight',
@@ -48,10 +39,6 @@ require('lazy').setup({
 --
   require 'kickstart/plugins/treesitter',
 
-  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
-  --
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
@@ -61,12 +48,9 @@ require('lazy').setup({
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
 
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
