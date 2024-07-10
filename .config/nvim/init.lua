be87420635d5ec906a7951e129ce987f39333c6e
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-07-10 14:38:05 -0700

    Moved options and keymaps to config module

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,16 +8,13 @@ vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
-require 'options'
+require 'config.options'
 
 -- [[ Basic Keymaps ]]
-require 'keymaps'
+require 'config.keymaps'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 require 'lazy-bootstrap'
 
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
