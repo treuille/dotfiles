65160b999bdaa36d653e7fc573388fe0cf634441
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-07-09 17:27:25 -0700

    Now really ready to trigger lazy.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,10 +14,10 @@ require 'options'
 require 'keymaps'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
--- require 'lazy-bootstrap'
+require 'lazy-bootstrap'
 
 -- [[ Configure and install plugins ]]
--- require 'lazy-plugins'
+require 'lazy-plugins'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
