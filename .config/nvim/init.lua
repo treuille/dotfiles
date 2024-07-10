4454ab6080570764f597687b265750ee2d522269
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-07-10 15:05:19 -0700

    Simplified directory structure kickstart -> vanilla lazy.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,5 @@
 -- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+-- NOTE: Must do before loading plugins or wrong leader will be used
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -13,8 +12,5 @@ require 'config.options'
 -- [[ Basic Keymaps ]]
 require 'config.keymaps'
 
--- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
-
--- [[ Configure and install plugins ]]
-require 'lazy-plugins'
+-- [[ Install and run `lazy.nvim` plugin manager ]]
+require 'config.lazy'
