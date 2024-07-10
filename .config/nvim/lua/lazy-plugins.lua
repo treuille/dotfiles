49815b8266135c0f30e57e2f360897fb46630c1b
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-07-09 17:38:48 -0700

    Installed the tokyonight color scheme

--- .config/nvim/lua/lazy-plugins.lua
+++ .config/nvim/lua/lazy-plugins.lua
@@ -39,20 +39,18 @@ require('lazy').setup({
 --  require 'kickstart/plugins/conform',
 --
 --  require 'kickstart/plugins/cmp',
---
---  require 'kickstart/plugins/tokyonight',
---
+
+  require 'kickstart/plugins/tokyonight',
+
 --  require 'kickstart/plugins/todo-comments',
 --
 --  require 'kickstart/plugins/mini',
 --
---  require 'kickstart/plugins/treesitter',
+  require 'kickstart/plugins/treesitter',
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
-
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
   --
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
