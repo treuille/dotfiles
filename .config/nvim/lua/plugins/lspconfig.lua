9d7e7ed79b986b2a4e4e2198d211108530c39831
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-07-11 00:49:13 -0700

    Configured LSP support for lua

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -5,9 +5,9 @@ return {
       'hrsh7th/cmp-nvim-lsp',
 
       -- -- Automatically install LSPs and related tools to stdpath for Neovim
-      -- { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
-      -- 'williamboman/mason-lspconfig.nvim',
-      -- 'WhoIsSethDaniel/mason-tool-installer.nvim',
+      { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
+      'williamboman/mason-lspconfig.nvim',
+      'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
       'j-hui/fidget.nvim',
@@ -17,11 +17,8 @@ return {
       'folke/neodev.nvim',
     },
 
-    config = function()
-     --  This function gets run when an LSP attaches to a particular buffer.
-      --    That is to say, every time a new file is opened that is associated with
-      --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-      --    function will be executed to configure the current buffer
+    config = function ()
+      -- This function gets run when an LSP attaches to a particular buffer.
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
@@ -94,38 +91,8 @@ return {
                 end
           })
 
-          -- -- The following two autocommands are used to highlight references of the
-          -- -- word under your cursor when your cursor rests there for a little while.
-          -- --    See `:help CursorHold` for information about when this is executed
-          -- --
-          -- -- When you move your cursor, the highlights will be cleared (the second autocommand).
-          -- local client = vim.lsp.get_client_by_id(event.data.client_id)
-          -- if client and client.server_capabilities.documentHighlightProvider then
-          --   local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-          --   vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-          --     buffer = event.buf,
-          --     group = highlight_augroup,
-          --     callback = vim.lsp.buf.document_highlight,
-          --   })
-          --
-          --   vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-          --     buffer = event.buf,
-          --     group = highlight_augroup,
-          --     callback = vim.lsp.buf.clear_references,
-          --   })
-          --
-          --   vim.api.nvim_create_autocmd('LspDetach', {
-          --     group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-          --     callback = function(event2)
-          --       vim.lsp.buf.clear_references()
-          --       vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-          --     end,
-          --   })
-          -- end
-
           -- The following autocommand is used to enable inlay hints in your
           -- code, if the language server you are using supports them
-          --
           -- This may be unwanted, since they displace some of your code
           if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
             map('<leader>th', function()
@@ -142,6 +109,10 @@ return {
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
+      -- These are the mason packages we want to install
+      local ensure_installed = {}
+
+      -- Setup basedpyright for Python projects
       require('lspconfig').basedpyright.setup({
         capabilities = capabilities,
         settings = {
@@ -151,62 +122,29 @@ return {
         }
       })
 
-      -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-      --
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-      local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
-
-        lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
+      -- Setup lua_ls for lua projects
+      require('lspconfig').lua_ls.setup({
+          capabilities = capabilities,
           settings = {
             Lua = {
               completion = {
                 callSnippet = 'Replace',
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = {
+                disable = { 'missing-fields' },
+                globals = { 'vim' },
+              },
             },
-          },
-        },
-      }
+          }
+      })
+      table.insert(ensure_installed, 'lua-language-server')
 
-      -- -- Ensure the servers and tools above are installed
-      -- --  To check the current status of installed tools and/or manually install
-      -- --  other tools, you can run
-      -- --    :Mason
-      -- --
-      -- --  You can press `g?` for help in this menu.
-      -- require('mason').setup()
-      --
-      -- -- You can add other tools here that you want Mason to install
-      -- -- for you, so that they are available from within Neovim.
-      -- local ensure_installed = vim.tbl_keys(servers or {})
-      -- vim.list_extend(ensure_installed, {
-      --   'stylua', -- Used to format Lua code
-      -- })
-      -- require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-      --
-      -- require('mason-lspconfig').setup {
+      -- Setup Mason
+      require('mason').setup()
+      require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
+      require('mason-lspconfig').setup()
+      -- {
       --   handlers = {
       --     function(server_name)
       --       local server = servers[server_name] or {}
@@ -221,4 +159,3 @@ return {
     end,
   },
 }
--- vim: ts=2 sts=2 sw=2 et
