8f6442bfa78312084c8b8748453e7c17b340c3e6
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-07-13 14:09:27 -0700

    Added some treesitter specific textobjects

---
+++ .config/nvim/lua/plugins/treesitter-textobjects.lua
@@ -0,0 +1,40 @@
+-- Adds the following treesitter-based text objects:
+--  * aF : function outer
+--  * iF : function inner
+--  * aB : block outer
+--  * iB : block inner
+--  * aC : comment outer
+--  * iC : comment inner
+--  * aL : loop outer
+--  * iL : loop inner
+return {
+  { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        textobjects = {
+          select = {
+            enable = true,
+            keymaps = {
+              -- Your custom capture.
+              -- ["aF"] = "@custom_capture",
+
+              -- Built-in captures.
+              ["aF"] = "@function.outer",
+              ["iF"] = "@function.inner",
+              ["aB"] = "@block.outer",
+              ["iB"] = "@block.inner",
+              ["aC"] = "@comment.outer",
+              ["iC"] = "@comment.inner",
+              ["aL"] = "@loop.outer",
+              ["iL"] = "@loop.inner",
+            },
+          },
+        },
+      }
+    end,
+  },
+}
