5db7d36ed47584489b9a02f1e21ee8c452e35bdd
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-07-14 00:38:00 -0700

    Only loading ChatGPT plugin if key file exists

--- .config/nvim/lua/plugins/chatgpt.lua
+++ .config/nvim/lua/plugins/chatgpt.lua
@@ -1,3 +1,8 @@
+-- Only load the plugin if the API key exists
+local api_key = vim.fn.expand("~/.config/nvim/chatgpt_nvim.txt")
+local api_key_exists = vim.fn.filereadable(api_key) == 1
+
+-- Proives a bunch of GenAI tools powered by OpenAI's GPT
 return {
   "jackMort/ChatGPT.nvim",
   dependencies = {
@@ -5,17 +10,18 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim"
   },
+  enabled = api_key_exists,
   event = "VeryLazy",
   keys = {
-    { '<leader>hh', '<cmd>ChatGPT<CR>', mode = 'n', desc = 'Open ChatGPT Session', },
-    { '<leader>hc', '<cmd>ChatGPTCompleteCode<CR>', mode = 'n', desc = "Edit with instruction" },
-    { '<leader>he', '<cmd>ChatGPTEditWithInstruction<CR>', mode = 'v', desc = "Edit with instruction" },
-    { '<leader>hd', '<cmd>ChatGPTRun docstring<CR>', mode = 'v', desc = "Docstring" },
-    { '<leader>ha', '<cmd>ChatGPTRun add_tests<CR>', mode = 'v', desc = "Add Tests" },
-    { '<leader>ho', '<cmd>ChatGPTRun optimize_code<CR>', mode = 'v', desc = "Optimize Code" },
-    { '<leader>hs', '<cmd>ChatGPTRun summarize<CR>', mode = 'v', desc = "Summarize" },
-    { '<leader>hf', '<cmd>ChatGPTRun fix_bugs<CR>', mode = 'v', desc = "Fix Bugs" },
-    { '<leader>hx', '<cmd>ChatGPTRun explain_code<CR>', mode = 'v', desc = "Explain Code" },
+    { '<leader>ah', '<cmd>ChatGPT<CR>', mode = 'n', desc = 'Open ChatGPT Session', },
+    { '<leader>ac', '<cmd>ChatGPTCompleteCode<CR>', mode = 'n', desc = "Edit with instruction" },
+    { '<leader>ae', '<cmd>ChatGPTEditWithInstruction<CR>', mode = 'v', desc = "Edit with instruction" },
+    { '<leader>ad', '<cmd>ChatGPTRun docstring<CR>', mode = 'v', desc = "Docstring" },
+    { '<leader>aa', '<cmd>ChatGPTRun add_tests<CR>', mode = 'v', desc = "Add Tests" },
+    { '<leader>ao', '<cmd>ChatGPTRun optimize_code<CR>', mode = 'v', desc = "Optimize Code" },
+    { '<leader>as', '<cmd>ChatGPTRun summarize<CR>', mode = 'v', desc = "Summarize" },
+    { '<leader>af', '<cmd>ChatGPTRun fix_bugs<CR>', mode = 'v', desc = "Fix Bugs" },
+    { '<leader>ax', '<cmd>ChatGPTRun explain_code<CR>', mode = 'v', desc = "Explain Code" },
   },
   cmd = {
     "ChatGPT",
@@ -25,7 +31,7 @@ return {
     "ChatGPTRun"
   },
   opts = {
-    api_key_cmd = 'cat /home/adrien/.config/nvim/chatgpt_nvim.txt',
+    api_key_cmd = 'cat ' .. api_key,
     popup_input = {
       submit = "<Enter>",
       submit_n = "<C-S-Enter>",
