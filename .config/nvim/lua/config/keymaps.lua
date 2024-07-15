9586a92ae7948a9d26cf37526067391701ae28d9
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-07-14 22:10:37 -0700

    Updated keymaps to avoid <ESC> collision

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,9 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
--- Set highlight on search, but clear on pressing <Esc> in normal mode
+-- Set highlight on search, but clear on pressing <leader>n in normal mode
 vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<leader>n', '<cmd>nohlsearch<CR>')
 
 -- Makes saving files easier
 vim.keymap.set('n', '<leader>u', ':update<cr>', {
@@ -11,22 +11,6 @@ vim.keymap.set('n', '<leader>u', ':update<cr>', {
   silent = true,
   desc = '[U]pdate file'
 })
-
--- -- Diagnostic keymaps - I don't believe this is necessary in neovim 0.10
--- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
--- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
--- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
--- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
-
--- -- Keybinds to make split navigation easier.
--- --  Use CTRL+<hjkl> to switch between windows
--- --
--- --  See `:help wincmd` for a list of all window commands
--- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
--- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
--- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
--- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
