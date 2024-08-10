fbfb687eb3e512f37f73d39b528eb80c206ce228
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-08-10 15:22:59 -0700

    Disabling markdown.lua because it was making my markdown files messy

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,9 +1,14 @@
 return {
-    'MeanderingProgrammer/markdown.nvim',
-    dependencies = {
-        'nvim-treesitter/nvim-treesitter',
-        'nvim-tree/nvim-web-devicons',
-    },
-    ft = { 'markdown' },
-    opts = {},
+  'MeanderingProgrammer/markdown.nvim',
+
+  -- Honestly, this plugin makes my markdown files look ugly.
+  -- Disabling for now.
+  enabled = false,
+
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons',
+  },
+  ft = { 'markdown' },
+  opts = {},
 }
