de5c16a8c13dc531d5e87cbd3c1c3acac06c1b2d
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-09-28 15:53:51 -0700

    No longer installing the GitHub command line tools

--- setup/setup_root.py
+++ setup/setup_root.py
@@ -60,19 +60,19 @@ def setup_root():
         ],
     )
 
-    # Install the latest GitHub Command line tools
-    setup_utils.cached_run(
-        "Installing GitHub Command line tools",
-        [
-            "apt-get install wget -y",
-            "mkdir -p -m 755 /etc/apt/keyrings",
-            "wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null",
-            "sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg",
-            'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null',
-            "sudo apt update",
-            "sudo apt install gh -y",
-        ],
-    )
+    # # Install the latest GitHub Command line tools
+    # setup_utils.cached_run(
+    #     "Installing GitHub Command line tools",
+    #     [
+    #         "apt-get install wget -y",
+    #         "mkdir -p -m 755 /etc/apt/keyrings",
+    #         "wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null",
+    #         "sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg",
+    #         'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null',
+    #         "sudo apt update",
+    #         "sudo apt install gh -y",
+    #     ],
+    # )
 
     # I nice TUI for GIT - need to test this
     setup_utils.cached_run(
