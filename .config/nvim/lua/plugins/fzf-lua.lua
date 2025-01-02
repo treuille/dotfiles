9199a37086672aa28868815637f2122013a57bd9
Adrien Treuille <adrien.g.treuille@gmail.com>
2025-01-02 10:18:20 -0800

    Cleaned up visual look of fzf-lua

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -20,14 +20,7 @@ return {
       { '<leader>sr', '<cmd>FzfLua resume<cr>', desc = '[S]earch [R]esume' },
       { '<leader>s.', '<cmd>FzfLua oldfiles<cr>', desc = '[S]earch Recent Files ("." for repeat)' },
       { '<leader><leader>', '<cmd>FzfLua buffers<cr>', desc = '[ ] Find existing buffers' },
-      -- { '<leader>ssd', '<cmd>FzfLua lsp_document_symbols<cr>', desc = '[S]earch [S]ymbols [D]ocument' },
-      {
-        '<leader>ssd',
-        function()
-          require('fzf-lua').lsp_document_symbols { fzf_cli_args = '--with-nth 1..' }
-        end,
-        desc = '[S]earch [S]ymbols [D]ocument',
-      },
+      { '<leader>ssd', '<cmd>FzfLua lsp_document_symbols<cr>', desc = '[S]earch [S]ymbols [D]ocument' },
       { '<leader>ssw', '<cmd>FzfLua lsp_workspace_symbols<cr>', desc = '[S]earch [S]ymbols [W]orkspace' },
       {
         '<leader>ssf',
@@ -49,37 +42,26 @@ return {
       },
     },
 
-    opts = {},
+    opts = {
+      winopts = {
+        border = 'rounded', -- 'none', 'single', 'double', 'thicc'
+        -- title_pos = 'left',
+        -- backdrop = 60,
+        treesitter = {
+          enabled = true,
+          fzf_colors = false,
+        },
+      },
+      preview = {
+        border = 'noborder', -- 'border'
+      },
+      defaults = {
+        file_icons = false,
+        actions = { ['ctrl-g'] = false },
+        cwd_prompt = false,
+        toggle_ignore_flag = '--no-ignore', -- flag toggled in `actions.toggle_ignore`
+        toggle_hidden_flag = '--hidden', -- flag toggled in `actions.toggle_hidden`
+      },
+    },
   },
 }
-
---       -- Old keymaps without the dropdown theme
---       -- vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
---       -- vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
---       -- vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
---       -- vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
---       -- vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
---       -- vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
---       -- vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
---       -- vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
---       -- vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
---       -- vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
---
---       -- Slightly advanced example of overriding default behavior and theme
---       vim.keymap.set('n', '<leader>/', function()
---         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
---         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
---           -- NOTE: Disabling tranparency because I find it hard to read
---           -- winblend = 10,
---           previewer = false,
---         })
---       end, { desc = '[/] Fuzzily search in current buffer' })
---
---       -- It's also possible to pass additional configuration options.
---       --  See `:help telescope.builtin.live_grep()` for information about particular keys
---       vim.keymap.set('n', '<leader>s/', function()
---         builtin.live_grep {
---           grep_open_files = true,
---           prompt_title = 'Live Grep in Open Files',
---         }
---       end, { desc = '[S]earch [/] in Open Files' })
