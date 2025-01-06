acd66a487465ff199d82536b3d14b13bc9af7641
Adrien Treuille <adrien.g.treuille@gmail.com>
2025-01-06 13:45:01 -0800

    Configuring copilot in plugins/copilot.lua
    Configuring copilot in plugins/copilot.lua

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,9 +1,17 @@
 -- This is a new AI plugin I'm playing with
+
+-- Place the plugin window depending on the aspect ration
+local width = vim.o.columns
+local height = vim.o.lines
+local window_position = (width / height) > 1.5 and 'right' or 'bottom'
+
+-- Configuration for the plugin
 return {
   'yetone/avante.nvim',
   event = 'VeryLazy',
   lazy = false,
   version = false, -- set this if you want to always pull the latest change
+
   opts = {
     -- add any opts here
     provider = 'claude', -- 'openai',
@@ -14,14 +22,18 @@ return {
     claude = {
       model = 'claude-3-5-sonnet-20241022',
     },
-    behaviour = {
-      auto_suggestions = true,
-    },
+
+    -- NOTE: Commented out becuase configuring diretly in zbirenbaum/copilot
+    -- behaviour = {
+    --   auto_suggestions = true,
+    -- },
+
     hints = {
       enabled = false,
     },
     windows = {
       width = 40,
+      position = window_position,
     },
   },
 
@@ -38,21 +50,6 @@ return {
     'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
 
     -- [optional] for providers='copilot'
-    {
-      'zbirenbaum/copilot.lua',
-      opts = {
-        filetypes = {
-          yaml = false,
-          markdown = false,
-          help = false,
-          gitcommit = false,
-          gitrebase = false,
-          hgcommit = false,
-          svn = false,
-          cvs = false,
-          ['.'] = false,
-        },
-      },
-    },
+    'zbirenbaum/copilot.lua',
   },
 }
