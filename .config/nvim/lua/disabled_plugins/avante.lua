ea23bd2ff354b3e648c30ae49320d184933232c2
Adrien Treuille <adrien.g.treuille@gmail.com>
2025-11-02 18:52:25 -0800

    Removing Avante because I can't get it to work properly.

--- .config/nvim/lua/disabled_plugins/avante.lua
+++ .config/nvim/lua/disabled_plugins/avante.lua
@@ -1,10 +1,5 @@
 -- This is a new AI plugin I'm playing with
 
--- Place the plugin window depending on the aspect ration
-local width = vim.o.columns
-local height = vim.o.lines
-local window_position = (width / height) > 1.5 and 'right' or 'top' -- 'bottom'
-
 -- Configuration for the plugin
 return {
   'yetone/avante.nvim',
@@ -13,28 +8,39 @@ return {
   version = false, -- set this if you want to always pull the latest change
 
   opts = {
-    -- add any opts here
     provider = 'claude', -- 'openai',
-    auto_suggestions_provider = 'copilot',
-    openai = {
-      model = 'gpt-4o',
+    -- auto_suggestions_provider = 'copilot',
+    providers = {
+      claude = {
+        endpoint = 'https://api.anthropic.com',
+        model = 'claude-sonnet-4-20250514',
+        timeout = 30000, -- Timeout in milliseconds
+        extra_request_body = {
+          temperature = 0.75,
+          max_tokens = 20480,
+        },
+      },
     },
-    claude = {
-      model = 'claude-3-5-sonnet-20241022',
-    },
+
+    -- openai = {
+    --   model = 'gpt-4o',
+    -- },
+    -- claude = {
+    --   model = 'claude-3-5-sonnet-20241022',
+    -- },
 
     -- NOTE: Commented out becuase configuring diretly in zbirenbaum/copilot
     -- behaviour = {
     --   auto_suggestions = true,
-    -- },
+    -- }
 
     hints = {
       enabled = false,
     },
-    windows = {
-      width = 40,
-      position = window_position,
-    },
+    -- windows = {
+    --   height = 20,
+    --   position = 'top',
+    -- },
   },
 
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
@@ -50,7 +56,7 @@ return {
     'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
     'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
 
-    -- [optional] for providers='copilot'
-    'zbirenbaum/copilot.lua',
+    -- -- [optional] for providers='copilot'
+    -- 'zbirenbaum/copilot.lua',
   },
 }
